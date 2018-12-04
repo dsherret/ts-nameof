ca0366c30b872d58a458714df2cfadb13976b08c
David Sherret <dsherret@gmail.com>
2018-12-03 22:57:17 -0500

    chore: Cleanup and use the printer less.

--- src/transformation/transforms.ts
+++ src/transformation/transforms.ts
@@ -1,18 +1,19 @@
 import * as ts from "typescript";
 import { throwError } from "../utils";
 import { isNameof, isNameofFull } from "./checks";
+import { getNumberFromExpression, getReturnStatementExpressionFromBlock } from "./getters";
 
 const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
 
 export function visitNode(visitingNode: ts.Node, sourceFile: ts.SourceFile) {
     if (isNameof(visitingNode))
-        return getLiteral(getNameofAsString(visitingNode));
+        return createLiteral(getNameofAsString(visitingNode));
     if (isNameofFull(visitingNode))
-        return getLiteral(getNameofFullAsString(visitingNode));
+        return createLiteral(getNameofFullAsString(visitingNode));
 
     return visitingNode;
 
-    function getLiteral(str: string | undefined) {
+    function createLiteral(str: string | undefined) {
         if (str == null || str.trim().length === 0)
             return throwError(`Could not resolve string from expression: ${getNodeText(visitingNode)}`);
 
@@ -20,23 +21,38 @@ export function visitNode(visitingNode: ts.Node, sourceFile: ts.SourceFile) {
     }
 
     function getNameofAsString(callExpr: ts.CallExpression) {
-        if (callExpr.arguments != null && callExpr.arguments.length > 0)
-            return getLastPart(getNodeParts(callExpr.arguments[0]));
-        else if (callExpr.typeArguments != null && callExpr.typeArguments.length > 0)
-            return getLastPart(getNodeParts(callExpr.typeArguments[0]));
-        return undefined;
+        const expression = getExpression();
+        return expression == null ? undefined : getNodeParts(expression).pop();
 
-        function getLastPart(parts: string[]) {
-            return parts[parts.length - 1];
+        function getExpression() {
+            if (callExpr.arguments != null && callExpr.arguments.length === 1)
+                return callExpr.arguments[0];
+            else if (callExpr.typeArguments != null && callExpr.typeArguments.length === 1)
+                return callExpr.typeArguments[0];
+            return undefined;
         }
     }
 
     function getNameofFullAsString(callExpr: ts.CallExpression) {
-        if (callExpr.arguments != null && shouldUseArguments())
-            return getPartsAsString(getNodeParts(callExpr.arguments[0]), getCount(callExpr.arguments[1]));
-        if (callExpr.typeArguments != null && callExpr.typeArguments.length > 0)
-            return getPartsAsString(getNodeParts(callExpr.typeArguments[0]), getCount(callExpr.arguments == null ? undefined : callExpr.arguments[0]));
-        return undefined;
+        const exprAndCount = getExpressionAndCount();
+        return exprAndCount == null ? undefined : getPartsAsString(getNodeParts(exprAndCount.expression), getCount(exprAndCount.count));
+
+        function getExpressionAndCount() {
+            if (callExpr.arguments != null && shouldUseArguments())
+                return { expression: callExpr.arguments[0], count: callExpr.arguments[1] };
+            if (callExpr.typeArguments != null && callExpr.typeArguments.length > 0)
+                return { expression: callExpr.typeArguments[0], count: callExpr.arguments == null ? undefined : callExpr.arguments[0] };
+            return undefined;
+
+            function shouldUseArguments() {
+                if (callExpr.arguments == null || callExpr.arguments.length === 0)
+                    return false;
+                if (callExpr.typeArguments == null || callExpr.typeArguments.length === 0)
+                    return true;
+
+                return ts.isFunctionLike(callExpr.arguments[0]);
+            }
+        }
 
         function getPartsAsString(parts: string[], count: number) {
             return getSubParts().join(".");
@@ -59,22 +75,11 @@ export function visitNode(visitingNode: ts.Node, sourceFile: ts.SourceFile) {
         function getCount(countExpr: ts.Expression | undefined) {
             if (countExpr == null)
                 return 0;
-            if (ts.isNumericLiteral(countExpr) || ts.isPrefixUnaryExpression(countExpr)) {
-                const result = parseInt(getNodeText(countExpr), 10);
-                if (!isNaN(result))
-                    return result;
-            }
 
-            return throwError(`Expected count to be a number, but was: ${getNodeText(countExpr)}`);
-        }
-
-        function shouldUseArguments() {
-            if (callExpr.arguments == null || callExpr.arguments.length === 0)
-                return false;
-            if (callExpr.typeArguments == null || callExpr.typeArguments.length === 0)
-                return true;
-
-            return ts.isFunctionLike(callExpr.arguments[0]);
+            const result = getNumberFromExpression(countExpr);
+            if (result == null)
+                return throwError(`Expected count to be a number, but was: ${getNodeText(countExpr)}`);
+            return result;
         }
     }
 
@@ -93,57 +98,59 @@ export function visitNode(visitingNode: ts.Node, sourceFile: ts.SourceFile) {
             return getQualifiedNameParts(node);
         if (ts.isTypeReferenceNode(node))
             return getNodeParts(node.typeName);
+        if (ts.isIdentifier(node))
+            return [node.text];
         return [getNodeText(node)];
-    }
 
-    function getFunctionExpressionParts(func: ts.FunctionExpression): string[] {
-        return getNodeParts(getReturnStatementExpressionFromBlock(func.body)).slice(1);
-    }
-
-    function getArrowFunctionParts(func: ts.ArrowFunction): string[] {
-        if (ts.isBlock(func.body))
-            return getNodeParts(getReturnStatementExpressionFromBlock(func.body)).slice(1);
-        return getNodeParts(func.body).slice(1);
-    }
-
-    function getReturnStatementExpressionFromBlock(block: ts.Block) {
-        for (const statement of block.statements) {
-            if (ts.isReturnStatement(statement) && statement.expression != null)
-                return statement.expression;
+        function getFunctionExpressionParts(func: ts.FunctionExpression): string[] {
+            return getNodeParts(getReturnStatementExpressionFromBlockOrThrow(func.body)).slice(1);
         }
 
-        return throwError(`Cound not find return statement with an expression in function expression: ${getNodeText(block)}`);
-    }
-
-    function getElementAccessExpressionParts(elementAccessExpr: ts.ElementAccessExpression): string[] {
-        if (ts.isElementAccessExpression(elementAccessExpr.expression))
-            return addArgTextToArray([...getElementAccessExpressionParts(elementAccessExpr.expression)]);
-        if (ts.isPropertyAccessExpression(elementAccessExpr.expression))
-            return addArgTextToArray([...getPropertyAccessExpressionParts(elementAccessExpr.expression)]);
-        return addArgTextToArray([...getNodeParts(elementAccessExpr.expression)]);
-
-        function addArgTextToArray(items: string[]) {
-            items[items.length - 1] += getArgumentExpression();
-            return items;
+        function getArrowFunctionParts(func: ts.ArrowFunction): string[] {
+            if (ts.isBlock(func.body))
+                return getNodeParts(getReturnStatementExpressionFromBlockOrThrow(func.body)).slice(1);
+            return getNodeParts(func.body).slice(1);
         }
 
-        function getArgumentExpression() {
-            if (elementAccessExpr.argumentExpression == null)
-                return "";
-            return `[${getNodeParts(elementAccessExpr.argumentExpression).join(".")}]`;
+        function getElementAccessExpressionParts(elementAccessExpr: ts.ElementAccessExpression): string[] {
+            return addArgTextToArray(getPartsForKind());
+
+            function getPartsForKind() {
+                if (ts.isElementAccessExpression(elementAccessExpr.expression))
+                    return [...getElementAccessExpressionParts(elementAccessExpr.expression)];
+                if (ts.isPropertyAccessExpression(elementAccessExpr.expression))
+                    return [...getPropertyAccessExpressionParts(elementAccessExpr.expression)];
+                return [...getNodeParts(elementAccessExpr.expression)];
+            }
+
+            function addArgTextToArray(items: string[]) {
+                items[items.length - 1] += getArgumentExpression();
+                return items;
+            }
+
+            function getArgumentExpression() {
+                if (elementAccessExpr.argumentExpression == null)
+                    return "";
+                return `[${getNodeParts(elementAccessExpr.argumentExpression).join(".")}]`;
+            }
         }
-    }
 
-    function getQualifiedNameParts(name: ts.QualifiedName): string[] {
-        if (ts.isQualifiedName(name.left))
-            return [...getQualifiedNameParts(name.left), ...getNodeParts(name.right)];
-        return [...getNodeParts(name.left), ...getNodeParts(name.right)];
-    }
+        function getQualifiedNameParts(name: ts.QualifiedName): string[] {
+            if (ts.isQualifiedName(name.left))
+                return [...getQualifiedNameParts(name.left), ...getNodeParts(name.right)];
+            return [...getNodeParts(name.left), ...getNodeParts(name.right)];
+        }
 
-    function getPropertyAccessExpressionParts(pae: ts.PropertyAccessExpression): string[] {
-        if (ts.isPropertyAccessExpression(pae.expression))
-            return [...getPropertyAccessExpressionParts(pae.expression), ...getNodeParts(pae.name)];
-        return [...getNodeParts(pae.expression), ...getNodeParts(pae.name)];
+        function getPropertyAccessExpressionParts(pae: ts.PropertyAccessExpression): string[] {
+            if (ts.isPropertyAccessExpression(pae.expression))
+                return [...getPropertyAccessExpressionParts(pae.expression), ...getNodeParts(pae.name)];
+            return [...getNodeParts(pae.expression), ...getNodeParts(pae.name)];
+        }
+
+        function getReturnStatementExpressionFromBlockOrThrow(block: ts.Block) {
+            return getReturnStatementExpressionFromBlock(block)
+                || throwError(`Cound not find return statement with an expression in function expression: ${getNodeText(block)}`);
+        }
     }
 
     function getNodeText(node: ts.Node) {
