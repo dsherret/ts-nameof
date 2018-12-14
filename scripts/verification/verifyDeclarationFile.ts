1597b744ed8493377e7be62809ecfc559e5c6b02
David Sherret <dsherret@gmail.com>
2018-12-13 21:19:26 -0500

    chore: Rename definitionFileTests.ts to declarationFileTests.ts

--- scripts/verification/verifyDeclarationFile.ts
+++ scripts/verification/verifyDeclarationFile.ts
@@ -3,7 +3,7 @@ import { Project } from "ts-simple-ast";
 export function verifyDeclarationFile() {
     const project = new Project();
     const declarationFile = project.addExistingSourceFile("ts-nameof.d.ts");
-    const declarationFileTests = project.addExistingSourceFile("lib/definitionFileTests.ts");
+    const declarationFileTests = project.addExistingSourceFile("lib/declarationFileTests.ts");
     const diagnostics = [...declarationFile.getPreEmitDiagnostics(), ...declarationFileTests.getPreEmitDiagnostics()];
 
     if (diagnostics.length > 0)
