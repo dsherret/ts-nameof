12bbf6c75a018dd42f58184fe056b43176d3b017
David Sherret <dsherret@gmail.com>
2019-07-02 22:01:02 -0400

    chore: Update global declaration file tests to match what's done on definitely typed.
    Will make the ExpectType work later.

--- shared/lib/global.tests.ts
+++ shared/lib/global.tests.ts
@@ -1,5 +1,5 @@
 /// <reference path="./global.d.ts" />
-/* tslint:disable */
+// tslint:disable-next-line no-namespace
 namespace TestNamespace {
     export interface TestType {
         prop: string;
@@ -7,49 +7,49 @@ namespace TestNamespace {
 }
 
 class TestClass {
-    prop1: string;
-    prop2: string;
+    prop1 = "";
+    prop2 = "";
 }
 
 // nameof tests
-nameof(TestClass);
-nameof<TestNamespace.TestType>();
-nameof<TestClass>(t => t.prop1);
+nameof(TestClass); // $ExpectType string
+nameof<TestNamespace.TestType>(); // $ExpectType string
+nameof<TestClass>(t => t.prop1); // $ExpectType string
 
 // nameof.full tests
 const testInstance = new TestClass();
-nameof.full(testInstance.prop1);
-nameof.full(testInstance.prop1, 1);
-nameof.full<TestNamespace.TestType>();
-nameof.full<TestNamespace.TestType>(1);
-nameof.full<TestClass>(t => t.prop1);
-nameof.full<TestClass>(t => t.prop1, 1);
+nameof.full(testInstance.prop1); // $ExpectType string
+nameof.full(testInstance.prop1, 1); // $ExpectType string
+nameof.full<TestNamespace.TestType>(); // $ExpectType string
+nameof.full<TestNamespace.TestType>(1); // $ExpectType string
+nameof.full<TestClass>(t => t.prop1); // $ExpectType string
+nameof.full<TestClass>(t => t.prop1, 1); // $ExpectType string
 
 // nameof.toArray tests
-nameof.toArray(testInstance.prop1);
-nameof.toArray(testInstance.prop1, testInstance.prop2);
-nameof.toArray<TestClass>(t => [t.prop1]);
+nameof.toArray(testInstance.prop1); // $ExpectType string[]
+nameof.toArray(testInstance.prop1, testInstance.prop2); // $ExpectType string[]
+nameof.toArray<TestClass>(t => [t.prop1]); // $ExpectType string[]
 
 // reference type test
 const myObj = { test: "" };
-nameof(myObj);
-nameof.full(myObj);
-nameof.toArray(myObj);
+nameof(myObj); // $ExpectType string
+nameof.full(myObj); // $ExpectType string
+nameof.toArray(myObj); // $ExpectType string[]
 
 // primitive type test
 const myStr = "";
-nameof(myStr);
-nameof.full(myStr);
-nameof.toArray(myStr);
+nameof(myStr); // $ExpectType string
+nameof.full(myStr); // $ExpectType string
+nameof.toArray(myStr); // $ExpectType string[]
 
 // null test
 const nullTypedVar = null;
-nameof(nullTypedVar);
-nameof.full(nullTypedVar);
-nameof.toArray(nullTypedVar);
+nameof(nullTypedVar); // $ExpectType string
+nameof.full(nullTypedVar); // $ExpectType string
+nameof.toArray(nullTypedVar); // $ExpectType string[]
 
 // undefined test
 const undefinedTypedVar = undefined;
-nameof(undefinedTypedVar);
-nameof.full(undefinedTypedVar);
-nameof.toArray(undefinedTypedVar);
+nameof(undefinedTypedVar); // $ExpectType string
+nameof.full(undefinedTypedVar); // $ExpectType string
+nameof.toArray(undefinedTypedVar); // $ExpectType string[]
